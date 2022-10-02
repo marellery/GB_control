0ab8980e4ef2e23cde6bc9b9f9c0c7a8e47ce13d
marellery <marellery@ya.ru>
2022-10-02 13:38:26 +0500

    написан метод

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
 ﻿string[] array = new string[5] {"123", "23", "hello", "world", "res"};
 
 Console.WriteLine("Первоначальный массив");
-Console.WriteLine(String.Join(", ", array));
+Console.WriteLine(String.Join("\t", array));
 Console.WriteLine();
+
+string[] resultArray = new string[array.Length];
+
+
+
+void SecondArrayWithIF(string[] array, string[] resultArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        resultArray[count] = array[i];
+        count++;
+        }
+    }
+}
+
+
 
