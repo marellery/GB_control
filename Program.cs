e80e719ac67b83150021c484ed5a8fef716cda7b
marellery <marellery@ya.ru>
2022-10-02 11:41:40 +0500

    Создан массив случайных чисел

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int size = 8;
+int [] array = new int [size];
+
+Console.WriteLine("Первоначальный массив");
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(10000);
+    Console.Write(array[i] + "\t");
+}
+Console.WriteLine();
+
+
+
+
